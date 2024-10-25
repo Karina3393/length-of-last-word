99ef9f9dc58bb6890489f0de829d15739a031aad
карина <@DESKTOP-FP9I0KA>
2024-10-25 13:24:58 +0300

    Add project files.

---
+++ length-of-last-word/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace length_of_last_word
+{
+    public class Solution
+    {
+        public int LengthOfLastWord(string s)
+        {
+           
+            s = s.Trim();
+
+           
+            int lastSpaceIndex = s.LastIndexOf(' ');
+
+            
+            if (lastSpaceIndex == -1)
+            {
+                return s.Length;
+            }
+
+            
+            return s.Length - lastSpaceIndex - 1;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            
+            Console.WriteLine(solution.LengthOfLastWord("Hello World")); 
+            Console.WriteLine(solution.LengthOfLastWord("   fly me   to   the moon  ")); 
+            Console.WriteLine(solution.LengthOfLastWord("luffy is still joyboy")); 
+            Console.WriteLine(solution.LengthOfLastWord("   ")); 
+        }
+    }
+}
